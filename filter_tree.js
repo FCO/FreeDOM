f34d62d0d17921de546113a30204af5cf7678f74
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2011-09-09 11:25:15 -0300

    FilterTree starting to work

--- filter_tree.js
+++ filter_tree.js
@@ -22,6 +22,72 @@ SubtractionCache.prototype = {
    }
 };
 
+function FilterPath() {
+   this.array = [];
+}
+
+FilterPath.prototype = {
+   constructor: "FilterPath",
+   array: [],
+   number_of_changes_to_transform_in: function(sub_path){
+      if(sub_path.constructor == "FilterPath")
+         path = clone(sub_path.array);
+      else if(sub_path.constructor() == "String")
+         path.push(sub_path);
+      else if(sub_path.length != null)
+         path = clone(sub_path);
+      return to_add(path, this.array).length + to_subtract(path, this.array).length;
+   },
+   toString: function() {
+      return this.string;
+   },
+   set string(str) {
+      this.array = str.split("::");
+   },
+   get string() {
+      return this.array.join("::");
+   },
+   push: function(sub_path){
+      var path = [];
+      if(sub_path.constructor == "FilterPath")
+         path = clone(sub_path.array);
+      else if(sub_path.constructor() == "String")
+         path.push(sub_path);
+      else if(sub_path.length != null)
+         path = clone(sub_path);
+      while(path.length > 0)
+         this.array.push(path.shift());
+   },
+   shift: function() {
+      return this.array.shift();
+   },
+   clone: function() {
+      var clone = new FilterPath();
+      clone.string = this.string;
+      return clone;
+   },
+   set_base: function(path) {
+      var my_path;
+      if(path.constructor == "FilterPath"){
+         my_path = clone(path.array);
+      } else {
+         my_path = clone(path);
+         var tmp = new FilterPath();
+         tmp.array = path;
+         path = tmp;
+      }
+      while(my_path.length > 0) {
+         var val = my_path.pop();
+         if(val > this.array[0])
+            throw "Base (" + path.string + ") should be smaller than the path (" + this.string + ")";
+         this.array.unshift(val);
+      }
+   },
+   get length() {
+      return this.array.length;
+   }
+};
+
 function clone(arr){
    var tmp = [];
    for(var i = 0; i < arr.length; i++)
@@ -40,7 +106,7 @@ function to_add(desired, existed) {
 function subtract_array(arr1, arr2) {
    var cache = SubtractionCache.get_instance();
    if(cache.exists(arr1, arr2)) {
-      return cache.get_cache(arr1, arr2);
+    return cache.get_cache(arr1, arr2);
    }
    var ret = [];
    for(var i = 0; i < arr1.length; i++){
@@ -71,22 +137,32 @@ FilterNode.prototype = {
    children: [],
    name: null,
    put_child: function(node) {
+      for(var i = 0; i < this.children.length; i++){
+         if(this.children[i].name > node.name){
+            this.children.splice(i, 0, node);
+            return;
+         }
+      }
       this.children.push(node);
    },
    put_on_path: function(node, path) {
    },
    has_child: function(child_name) {
-      for(var i = 0; i < this.children.length; i++) {
+      for(var i = 0; i < this.children.length && this.children[i].name <= child_name; i++) {
          if(this.children[i].name == child_name)
             return true;
       }
       return false;
    },
    kid_path: function(name) {
-      var ret = [];
-      if(this.has_child(name))
-         return [ this.get_child(name).name ];
+      var ret = new FilterPath();
+      if(this.has_child(name)) {
+         var path = new FilterPath();
+         path.push([ this.get_child(name).name ]);
+         return path;
+      }
       for(var i = 0; i < this.children.length; i++) {
+      //for(var i = 0; i < this.children.length && this.children[i].name <= name; i++) {
          if(this.children[i].has_kid(name)) {
             ret.push(this.children[i].name);
             var tmp = [];
@@ -95,11 +171,7 @@ FilterNode.prototype = {
                tmp = tmp_val;
             else
                tmp.push(tmp_val);
-            while(tmp.length > 0) {
-               var val = tmp.shift();
-               if(val != null)
-                  ret.push(val);
-            }
+            ret.push(val);
             return ret;
          }
       }
@@ -112,14 +184,14 @@ FilterNode.prototype = {
          level = -1;
       if(level == 0)
          return false;
-      for(var i = 0; i < this.children.length; i++) {
+      for(var i = 0; i < this.children.length && this.children[i].name <= name; i++) {
          if(this.children[i].has_kid(name, level - 1))
             return true;
       }
       return false;
    },
    get_child: function(child_name) {
-      for(var i = 0; i < this.children.length; i++) {
+      for(var i = 0; i < this.children.length && this.children[i].name <= child_name; i++) {
          if(this.children[i].name == child_name)
             return this.children[i];
       }
@@ -204,13 +276,95 @@ FilterNode.prototype = {
       }
       return array;
    },
+   get_partial_paths: function(path) {
+      window.console.log(this.name + ".get_partial_paths(" + path + ")");
+      var paths = [];
+      var obj_path;
+      if(path.constructor == "FilterPath") {
+         obj_path = path.clone();
+      } else {
+         obj_path = new FilterPath();
+         obj_path.array = path;
+      }
+      var first = obj_path.shift();
+
+      window.console.log("first: " + first);
+      var kids = this.get_every_kid(first);
+      window.console.log("obj_path: " + obj_path.length);
+      if(obj_path.length <= 0) {
+         window.console.log("on if");
+         window.console.log("kids: " + kids);
+         return kids;
+      }
+      var kids2path = clone(kids);
+      while(kids2path.length > 0)
+         paths.push(kids2path.shift());
+      for(var i = 0; i < kids.length; i++) {
+         var internal_kids = this.get_path(kids[i].clone()).get_partial_paths(obj_path);
+         //var internal_kids = this.get_path(kids[i].clone()).get_every_kid(path);
+         window.console.log("internal_kids: " + internal_kids);
+         for(var j = 0; j < internal_kids.length; j++) {
+            var kid = kids[i].clone();
+            internal_kids[j].set_base(kid);
+            paths.push(internal_kids[j]);
+         }
+      }
+      return paths;
+   },
    //get_partial_paths: function(path) {
+   //   window.console.log(this.name + ".get_partial_paths(" + path + ")");
    //   var paths = [];
-   //   for(var i = 0; i < path.length; i++) {
-   //      var every_kid = this.get_every_kid(path[i]);
-   //      
+   //   var first = path.shift();
+   //   var every_kid = this.get_every_kid(first);
+   //   for(var i = 0; i < every_kid.length; i++){
+   //      var actual_path = every_kid[i];
+   //      window.console.log("actual_path: " + actual_path);
+   //      if(path.length > 0){
+   //         window.console.log("dentro do if");
+   //         var tmp = this.get_path(actual_path).get_partial_paths(path);
+   //         window.console.log("returned: " + tmp);
+   //         for(var j = 0; j < tmp.length; j++){
+   //            var a_path = clone(actual_path);
+   //            window.console.log("a_path: " + a_path);
+   //            for(var k = 0; k < tmp[j].length; k++){
+   //               a_path.push(tmp[j][k]);
+   //            }
+   //            paths.push(a_path);
+   //         }
+   //      } else {
+   //         paths.push(actual_path);
+   //      }
    //   }
+   //   return paths;
    //},
+//   get_partial_paths: function(path) {
+//      var paths = [];
+//      while(path.length > 0) {
+//         var first = path.shift();
+//         var every_kid = this.get_every_kid(first);
+//window.console.log("every_kid");
+//window.console.log(every_kid);
+//         for(var i = 0; i < every_kid.length; i++){
+//            var tmp_path = clone(every_kid[i]);
+//            var kid = this.get_path(every_kid[i]);
+//            if(paths.length > 0) {
+//               var tmp = kid.get_partial_paths(path);
+//               for(var j = 0; j < tmp.length; j++){
+//                  for(var k = 0; k < tmp[j].length; k++){
+//                     tmp_path.push(tmp[j][k]);
+//                  }
+//                  paths.push(tmp_path);
+//               }
+//            } else {
+//               tmp_path.push(kid.name);
+//               paths.push(tmp_path);
+//            }
+//         }
+//      }
+//window.console.log("returning paths");
+//window.console.log(paths);
+//      return paths;
+//   },
    get_closer_path: function(path) {
       var paths = this.get_partial_paths(path);
       var possibilities = [];
@@ -224,27 +378,23 @@ FilterNode.prototype = {
    },
    get_every_kid: function(name) {
       var paths = [];
-      var begin = [];
-      if(this.name != null)
-         begin.push(this.name);
-      for(var i = 0; i < this.children.length; i++) {
-         var new_path = clone(begin);
-         if(this.children[i].name == name) {
-            if(this.name != null)
-               paths.push([this.name, name]);
-            else
-               paths.push([name]);
-         } else if(this.children[i].has_kid(name)) {
-            var returned_paths = this.children[i].get_every_kid(name);
-            for(var j = 0; j < returned_paths.length; j++){
-               for(var k = 0; k < returned_paths[j].length; k++){
-                  new_path.push(returned_paths[j][k]);
-               }
-               paths.push(new_path);
+      if(this.has_child(name)){
+         var tmp = new FilterPath();
+         tmp.push([name]);
+         paths.push(tmp);
+      }
+      for(var i = 0; i < this.children.length && this.children[i].name <= name; i++){
+         if(this.children[i].has_kid(name)) {
+            var kids = this.children[i].get_every_kid(name);
+            for(var j = 0; j < kids.length; j++) {
+               var base_path = new FilterPath();
+               base_path.push([ this.children[i].name ]);
+               base_path.push(kids[j]);
+               paths.push(base_path);
             }
          }
       }
-      return paths;
+      return paths
    },
    force_partial_path: function(path){
 //window.console.log(path);
